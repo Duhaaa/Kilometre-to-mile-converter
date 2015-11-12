d6327d840b8e0ff39836a899978f0c99e005d1d8
Duhaaa <duhaokur@gmail.com>
2015-11-12 10:45:36 +0100

    A simple kilometer to mile converter.

---
+++ Converter2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Converter2
+{
+    public partial class Form1 : Form
+    {   
+
+        public Form1()
+        {
+            //Initializes the form
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float f;
+            string input = mileBox.Text;
+            
+            //If the user didn't enter a digit
+            if (!float.TryParse(input, out f))
+            {
+                MessageBox.Show("Only numbers are allowed!");
+            }
+            //else convert the input
+            else
+            {
+                double calc = double.Parse(input);
+                double sum = Math.Round(calc * 1.60934, 2);
+                label3.Text = sum.ToString();
+            }
+        } 
+    }
+}
